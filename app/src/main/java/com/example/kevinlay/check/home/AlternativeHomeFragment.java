d354919ff2b7317ad2079e6bb0aea8384183dfb7
Kevin Lay <kevinlay689@yahoo.com>
2017-12-15 14:56:45 -0800

    -Added Alternative Home Fragment prototype

---
+++ app/src/main/java/com/example/kevinlay/check/home/AlternativeHomeFragment.java
@@ -0,0 +1,29 @@
+package com.example.kevinlay.check.home;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kevinlay.check.R;
+
+/**
+ * Created by kevinlay on 12/15/17.
+ */
+
+public class AlternativeHomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_alternative_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
